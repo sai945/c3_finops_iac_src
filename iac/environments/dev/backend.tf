72a51be0a37dcefd29c5b8a3772fb0b8c4979fac
saikumar <61767599+sai945@users.noreply.github.com>
2025-09-16 18:00:48 +0530

    Update backend.tf

--- iac/environments/dev/backend.tf
+++ iac/environments/dev/backend.tf
@@ -1,9 +1,5 @@
 terraform {
-  backend "s3" {
-    bucket  = "c3ops.io"                   # Your S3 bucket name
-    key     = "aifinops-terraform.tfstate" # Path to the state file
-    region  = "us-east-1"                  # AWS region
-    encrypt = true                         # Encrypt the state file
-
+  backend "local" {
+    path = "relative/path/to/terraform.tfstate"
   }
 }
